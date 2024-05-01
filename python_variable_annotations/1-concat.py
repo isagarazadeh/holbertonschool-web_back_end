4ce28bcae90a1479e8ab22112aa8d5e526881a3a
root <garazadeisa625@gmail.com>
2024-05-01 13:34:39 +0000

    second task

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,7 +2,7 @@
 """Variable Annotations"""
 
 
-def concat(str1: str, str2: str) ->str:
+def concat(str1: str, str2: str) -> str:
     """concatenate string"""
     summa = str1 + str2
     return summa
