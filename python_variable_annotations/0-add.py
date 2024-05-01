4e93d37f4760498ab956cf81c3eccd41821e9df2
root <garazadeisa625@gmail.com>
2024-05-01 13:00:03 +0000

    first task

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 """Variable Annotations"""
 
-#def fn(a, b):
-#    return a + b
-num = [a, b] 
-
-num = list(map(float, num))
-
-total = reduce(lambda a, b: a + b, num)
-
-print(total)
+def (a: float, b: float) --> float
+return a + b
