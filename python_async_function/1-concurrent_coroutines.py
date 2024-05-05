55418bea7861020c30504c3be9e5864602440009
root <garazadeisa625@gmail.com>
2024-05-05 17:12:40 +0000

    1st task

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Asynchronous"""
+import asyncio
+import random
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """function"""
+    for i in range n:
+        waits = [wait_random(max_delay)]
+        dely = []
+    for i in asyncio.as_completed(waits):
+        delay = await i
+        dely.append(delay)
+
+    return delays
