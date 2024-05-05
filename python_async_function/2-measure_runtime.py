665b507845985019ed713175f7bfffc3a680ac73
root <garazadeisa625@gmail.com>
2024-05-05 18:32:25 +0000

    2nd task

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """"""
+    """Related to the function part"""
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     total = time.time() - start
