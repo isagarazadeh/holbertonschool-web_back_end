9c2c162427d49e6e016c9255c0726240ba4ac5a7
root <garazadeisa625@gmail.com>
2024-05-05 18:00:03 +0000

    3rd task

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""asynchronous"""
+import asyncio
+import time
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> int:
+    """task"""
+    return asyncio.Task
