be40cde4de93daea9c21e6d0484160e62b45525d
root <garazadeisa625@gmail.com>
2024-05-05 18:31:08 +0000

    4th task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Task"""
+    """Related to the function part"""
     waits = [task_wait_random(max_delay) for i in range(0, n)]
     delays = []
     for i in asyncio.as_completed(waits):
