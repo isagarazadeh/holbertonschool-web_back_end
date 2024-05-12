c552c03fd4c2756fce7ccacf5ca1b91f1bb34403
root <garazadeisa625@gmail.com>
2024-05-12 18:06:10 +0000

    12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
-"""Example"""
-from pymongo import MongoClient
+"""Documented module"""
+import pymongo
+
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx = client.logs.nginx
-    find = {"method": "GET", "path": "/status"}
-    print(f"{nginx.count_documents({})} logs")
+    """Log stats"""
+    client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+    collection = client.logs.nginx
+
+    doc_count = collection.estimated_document_count()
+    print("{} logs".format(doc_count))
+
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        dictionary = {"method": method}
-        print(f'\tmethod {method}: {nginx.count_documents(dictionary)}')
-    print(f'{nginx.count_documents(find)} status check')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+
+    status_count = collection.count_documents(
+        {
+            "method": "GET",
+            "path": "/status",
+        }
+    )
+
+    print("{} status check".format(status_count))
