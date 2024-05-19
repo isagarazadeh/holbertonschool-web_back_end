7adbfee0a41af8bebed2cd020ea2a2e15d17ff9b
root <garazadeisa@gmail.com>
2024-05-19 19:42:53 +0000

    4th task

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+/* eslint no-underscore-dangle: 0 */
+/* eslint-disable no-unused-vars */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._currency = currency;
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
