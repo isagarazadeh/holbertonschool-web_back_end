ae5118e4d952adca3097f8ca797c5fe8e618c847
root <garazadeisa625@gmail.com>
2024-05-19 10:40:33 +0000

    array

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -4,5 +4,5 @@ export default function initializeRooms() {
   const object1 = new ClassRoom(19);
   const object2 = new ClassRoom(20);
   const object3 = new ClassRoom(34);
-  return [object1, object2, object3]
+  return [object1, object2, object3];
 }
